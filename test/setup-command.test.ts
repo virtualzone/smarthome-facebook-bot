826c6121b66b80fb168b6751e96e4239b33bac8b
Heiner Peuser <heiner.peuser@gmail.com>
2017-01-04 12:36:08 +0100

    Fixed tests

--- test/setup-command.test.ts
+++ test/setup-command.test.ts
@@ -1,26 +1,35 @@
 import { expect } from "chai";
 import "mocha";
 
+import { User } from "../src/model/user";
 import { CommandExecutor } from "../src/command/command-executor";
 
 describe("SetupCommand", () => {
-    it("should interpret 'setup' with username and password correctly", () => {
-        let result: string = CommandExecutor.execute(null, "setup test http://my-smarthome:8080/fhem john doE123");
-        expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=john; password=doE123");
+    it("should interpret 'setup' with username and password correctly", (done) => {
+        CommandExecutor.execute(new User("test"), "setup test http://my-smarthome:8080/fhem john doE123").then(result => {
+            expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=john; password=doE123");
+            done();
+        });
     });
 
-    it("should interpret 'setup' without username and password correctly", () => {
-        let result: string = CommandExecutor.execute(null, "seTup Test http://my-smarthome:8080/fhem");
-        expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=; password=");
+    it("should interpret 'setup' without username and password correctly", (done) => {
+        CommandExecutor.execute(new User("test"), "seTup Test http://my-smarthome:8080/fhem").then(result => {
+            expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=; password=");
+            done();
+        });
     });
 
-    it("should interpret 'set up' with username and password correctly", () => {
-        let result: string = CommandExecutor.execute(null, "Set up test http://my-smarthome:8080/fhem john doE123");
-        expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=john; password=doE123");
+    it("should interpret 'set up' with username and password correctly", (done) => {
+        CommandExecutor.execute(new User("test"), "Set up test http://my-smarthome:8080/fhem john doE123").then(result => {
+            expect(result).to.equal("Simulated setup of url=http://my-smarthome:8080/fhem; username=john; password=doE123");
+            done();
+        });
     });
 
-    it("should return an error for unknown systems", () => {
-        let result: string = CommandExecutor.execute(null, "setup nothingexists http://my-smarthome:8080/fhem john doE123");
-        expect(result).to.contain("I'm sorry! I can't work with that system yet.");
+    it("should return an error for unknown systems", (done) => {
+        CommandExecutor.execute(new User("test"), "setup nothingexists http://my-smarthome:8080/fhem john doE123").then(result => {
+            expect(result).to.contain("I'm sorry! I can't work with that system yet.");
+            done();
+        });
     });
 });
